cc6e1919403e9e62487f23951a86af444041e7bb
yager36 <44434281+yager36@users.noreply.github.com>
2018-12-05 20:15:48 +0200

    Add files via upload

---
+++ Cookies_Class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Cookie
+    {
+        private bool gluten;
+
+        public bool Gluten
+        {
+            get { return gluten; }
+            set { gluten = value; }
+        }
+
+        private int eggs;
+
+        public int Eggs
+        {
+            get { return eggs; }
+            set { eggs = value; }
+        }
+
+        private Double sugar;
+
+        public Double Sugar
+        {
+            get { return sugar; }
+            set { sugar = value; }
+        }
+
+        private Double flour;
+
+        public Double Flour
+        {
+            get { return flour; }
+            set { flour = value; }
+        }
+        public void printCookie()
+        {
+
+            Console.WriteLine($"You have this amount of groceries to make a cookie:\n" +
+                              $"gluten {gluten}\n" +
+                              $"{Eggs} eggs\n" +
+                              $"{sugar} sugar\n" +
+                              $"{flour} flour\n");
+
+        }
+
+    }
+}
+
